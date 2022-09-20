7ce63be9d3aa8f58aede827db17632f6dba6721a
Shirley Alus <shirleyal@mta.ac.il>
2022-09-20 18:32:03 +0300

    Basic Game works

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{    
+    [SerializeField] private GameObject m_Aim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 4f, Screen.height / 2f, 0f));
+
+             if (Physics.Raycast(ray, out hit))
+             {
+                 if (hit.collider.gameObject.name.ToLower().Contains("top"))
+                 {
+                     
+                 }
+                 else
+                 {
+                     if (hit.collider.gameObject.name.ToLower().Contains("start"))
+                     {
+                         SceneManager.LoadScene(1);
+                     }
+                 }
+            }
+        }
+    }
+}
--- Assets/Scripts/MainPlayerController.cs
+++ Assets/Scripts/MainPlayerController.cs
@@ -7,8 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class MainPlayerController : MonoBehaviour
 {
-    private bool m_isInGameScene = false;
-
     private bool m_isGameStarted = false;
     
     private float m_timeCounter = 0.0f;
@@ -26,16 +24,8 @@ public class MainPlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (SceneManager.GetActiveScene().name.ToLower().Contains("game"))
-        {
-            m_isInGameScene = true;
-        }
-
-        if (m_isInGameScene)
-        {
-            m_Aim.SetActive(false);
-            StartCoroutine(countDown(3.0f));
-        }
+        m_Aim.SetActive(false);
+        StartCoroutine(countDown(3.0f));
     }
 
     IEnumerator countDown(float countDownAmount)
@@ -55,43 +45,18 @@ public class MainPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!m_isInGameScene)
-        {//Main Menu
-            if (Input.anyKeyDown)
-            {
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2f,Screen.height/2f,0f));
-                if (Physics.Raycast(ray, out hit))
-                {
-                    if (hit.collider.gameObject.name.Contains("Exit"))
-                    {
-                        Application.Quit();
-                    }
-                    else
-                    {
-                        if (hit.collider.gameObject.name.Contains("Start"))
-                        {
-                            SceneManager.LoadScene(1);
-                            m_isInGameScene = true;
-                        }
-                    }
-                }
-            }
+        if (m_isGameStarted)
+        {
+            m_timeCounter += Time.deltaTime;
+            m_GameTimer.text = "" + m_timeCounter + " s";
         }
-        else
-        {//Game Scene
-            if (m_isGameStarted)
-            {
-                m_timeCounter += Time.deltaTime;
-                m_GameTimer.text = "" + m_timeCounter + " s";
-            }
-            
-            if (Input.anyKeyDown)
-            {
-                GameObject bulletRef =  Instantiate(m_bullet, transform.position, Quaternion.identity, transform);
-                Destroy(bulletRef,5f);
-                bulletRef.GetComponent<Rigidbody>().AddForce(transform.forward * m_forcePower);
-            }
+
+        if (Input.anyKeyDown)
+        {
+            GameObject bulletRef = Instantiate(m_bullet, transform.position, Quaternion.identity, transform);
+            Destroy(bulletRef, 5f);
+
+            bulletRef.GetComponent<Rigidbody>().AddForce(m_Aim.transform.forward * m_forcePower);
         }
     }
 }
